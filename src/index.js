a6b98132645fa63ea86ca58dd8e1b284963b7cb7
Kamesas <semakaleksandr2014@gmail.com>
2021-07-08 17:57:13 +0300

    add dynamic page

--- src/index.js
+++ src/index.js
@@ -5,3 +5,29 @@ import "./sass/main.scss";
 console.log(sum(2, 2));
 
 console.log("main.js - common js file");
+
+console.log("window", window.location.search);
+
+const pages = {
+  post1: {
+    title: 'Post1',
+    h1: 'Heading post 1',
+    p: "Paragraph post 1"
+  },
+  post2: {
+    title: 'Post2',
+    h1: 'Heading post 2',
+    p: "Paragraph post 2"
+  }
+}
+
+if(window.location.search.includes('?_')) {
+  const page = pages[`${window.location.search.slice(2)}`];
+  const heading = document.getElementById('post-heading');
+  const content = document.getElementById('post-content');
+
+  document.title = page.title;
+  
+  heading.innerHTML = page.h1;
+  content.innerHTML = page.p;
+}
